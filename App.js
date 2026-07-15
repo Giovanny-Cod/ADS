e273f5e0fb4c933560f3704b3cb1de59e3485b65
Giovanny-Cod <giovanysk88@gmail.com>
2026-07-15 14:32:14 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Plataforma de Inclusão Digital</h1>
+      <p>Projeto acadêmico para democratização de acesso a conteúdos educacionais.</p>
+    </div>
+  );
+}
+
+export default App;
